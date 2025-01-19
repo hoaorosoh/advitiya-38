7ccf5c7b63f98a8e56f2b9bbde04bf2c3ffcc755
Aditya Verma <adityaverma10328@gmail.com>
2025-01-19 21:51:21 +0530

    some changes in js

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 let express = require('express');
-let mysql = require('mysql');
+//let mysql = require('mysql');
 let app = express();
 let path = require('path');
+/*const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 's3kreee7',
+    database: 'my_db'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err) throw err
+
+        console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+*/
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
