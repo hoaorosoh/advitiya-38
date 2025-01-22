a7cce3a7360974874af1fda1e27306fffd4b8d02
Aditya Verma <adityaverma10328@gmail.com>
2025-01-22 23:16:00 +0530

    made as much as we could. adding final finishes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 //const fileUpload = require('express-fileupload');
 const app = express();
 const path = require('path');
-/*const mysql = require('mysql')
+const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: '38',
@@ -20,10 +20,8 @@ connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
         console.log(rows.length, fields.length, err)
 })
 
-connection.end()
-*/
-
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.urlencoded({ extended: true }));
 //app.use(fileUpload());
 
 app.set('view engine', 'ejs');
@@ -34,29 +32,98 @@ app.get('/', (req, res) => {
 });
 
 app.get('/talent', (req, res) => {
-    res.render('ClientBrowse', {foo: "FOO"});
+    res.redirect('/talent/browse');
 });
 
 app.get('/talent/browse', (req, res) => {
-    res.render('ClientBrowse', {foo: "FOO"});
+    connection.query("SELECT * FROM Talent", (err, result, fields) => {
+        if (err) throw err;
+
+        console.log(fields);
+        console.log(result);
+        res.render('ClientBrowse', {result});
+    });
 });
 
 app.get('/talent/register', (req, res) => {
     res.render('TalentRegistration', {foo: "FOO"});
 });
 
-/*app.post('/talent/register', (req, res) => {
-    console.log(req.query);
-    let uploadedFile = req.files.resume;
-    console.log(`File Name: ${uploadedFile.name}`);
-    console.log(`File Size: ${uploadedFile.size}`);
-    /*let fpath = 'assets/pfps/' + Math.random().toString(36).substring(2,10);
-    req.files.resume.mv(fpath, (err) => {
-        console.log(err);
-        if (err) throw err;
-    });*/
-    /*res.redirect('/');
+app.get('/client/register', (req, res) => {
+    res.render('ClientRegistration', {foo: "FOO"});
+});
+
+app.post('/client/register', (req, res) => {
+    console.log(req.body);
+
+
+    /*let uploadedFile = req.files.resume;
+     c onsole.log(`File *Name: ${uploadedFile.name}`);
+console.log(`File Size: ${uploadedFile.size}`);
+/*lest fpath = 'assets/pfps/' + Math.random().toString(36).substring(2,10);
+ r eq.files.resume.m*v(fpath, (err) => {
+ console.log(err);
+if (err) throw err;
 });*/
+
+let nextID;
+connection.query("SELECT TalentID FROM Talent", (err, result, fields) => {
+    if (err) throw err;
+
+    nextID = result.length + 1;
+    console.log(nextID);
+})
+
+let query = `
+INSERT INTO Talent (Name, Phone, Email)
+VALUES (?, ?, ?)
+`;
+
+const values = [
+    req.body.name,
+    req.body.number,
+    req.body.email
+];
+
+connection.query(query, values, (err) => {
+    if (err) throw err;
+    console.log(`Added one talent: ${req.body.name}`);
+});
+
+
+res.redirect('/');
+});
+
+app.post('/talent/register', (req, res) => {
+    console.log(req.body);
+    let nextID;
+    connection.query("SELECT TalentID FROM Talent", (err, result, fields) => {
+        if (err) throw err;
+
+        nextID = result.length + 1;
+        console.log(nextID);
+    })
+
+    let query = `
+    INSERT INTO Talent (Name, Phone, Email, Skills)
+    VALUES (?, ?, ?, ?)
+    `;
+
+    const values = [
+         req.body.name,
+         req.body.number,
+         req.body.email,
+         req.body.skills
+    ];
+
+    connection.query(query, values, (err) => {
+        if (err) throw err;
+        console.log(`Added one client: ${req.body.name}`);
+    });
+
+
+    res.redirect('/');
+});
 
 app.post('/talent/register/submit', (req, res) => {
     res.redirect('/');
